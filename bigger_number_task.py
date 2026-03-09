11a115cbd247a1581bef546b881c0c831c2709d7
james malitan <jamesmalitan21@gmail.com>
2026-03-09 18:55:24 +0800

    program 1

---
+++ bigger_number_task.py
@@ -0,0 +1,10 @@
+ # think and crete a program that ask user ro input two numbers.
+ #input1
+first_number = float(input("Enter first number: "))
+#input2
+second_number = float(input("Enter second number: "))
+ # last step is to compare the 2 numbers
+if first_number > second_number:
+   print(first_number, "is bigger than ", second_number)
+else:
+   print(second_number, "is bigger than ", first_number)
