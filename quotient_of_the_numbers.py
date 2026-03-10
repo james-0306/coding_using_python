7b9ec597bbf0a78ae4373316c33495d495454fe0
james malitan <jamesmalitan21@gmail.com>
2026-03-10 08:47:01 +0800

    prog 5

---
+++ quotient_of_the_numbers.py
@@ -0,0 +1,8 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# Input 1
+first_number = float(input("Enter the first number: "))
+# Input 2
+second_number = float(input("Enter the second number: "))
+# create a program that will print the quotient of the number
+quotient_of_the_number = first_number / second_number
+print(quotient_of_the_number)
