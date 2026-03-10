438c77b1f5c7b48b7a942278634df6076e757d9c
james malitan <jamesmalitan21@gmail.com>
2026-03-10 08:23:45 +0800

    prog 2

---
+++ equal_number_task.py
@@ -0,0 +1,11 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+# Input number 1
+first_number = float(input("Enter the first number: "))
+# Input number 2
+second_number = float(input("Enter the second number: "))
+# analyze the number if they are the same and print equal
+if first_number == second_number:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
+
