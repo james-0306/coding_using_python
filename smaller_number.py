1a97bf2c707de7bce27f02379cd5d9d8991b1124
james malitan <jamesmalitan21@gmail.com>
2026-03-11 07:19:14 +0800

    prog 11

---
+++ smaller_number.py
@@ -0,0 +1,10 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+#Input 1
+first_number = float(input("First number: "))
+#Input 2
+second_number = float(input("Second number: "))
+#analyze the smaller number and print it
+if first_number < second_number:
+    print("The smaller number is: ", first_number)
+else:
+    print("the smaller number is: ", second_number)
