e6acde12bf61ae1036151583f1ab7b1614f732c0
james malitan <jamesmalitan21@gmail.com>
2026-03-10 20:05:54 +0800

    prog 9

---
+++ even_numbers.py
@@ -0,0 +1,7 @@
+#Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+#Make an range program 0 - 100
+even_numbers = 0
+for i in range(0, 101):
+# analyze every even numbers and print it
+    if i % 2 == 0:
+     print(i)
