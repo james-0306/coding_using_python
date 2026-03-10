19f5e77ccfae65884c436f5926de346ce33769f7
james malitan <jamesmalitan21@gmail.com>
2026-03-10 17:36:52 +0800

    prog 6

---
+++ exponent_number.py
@@ -0,0 +1,8 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# Input 1
+first_number = float(input("Enter the first number: "))
+# Input 2
+second_number = float(input("Enter the second number: "))
+# create a program that will print the result when the first number is raised to the second number
+exponent_of_the_number = first_number ** second_number
+print(exponent_of_the_number)
