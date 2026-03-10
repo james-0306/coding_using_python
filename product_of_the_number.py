9866f75920d683d7e30d343f841c12ad0d32d940
james malitan <jamesmalitan21@gmail.com>
2026-03-10 08:42:21 +0800

    prog 4

---
+++ product_of_the_number.py
@@ -0,0 +1,8 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+# Input 1
+first_number = float(input("Enter the first number: "))
+# Input 2
+second_number = float(input("Enter the second number: "))
+# create a program that will print the product of the numbers
+product_of_numbers = first_number * second_number
+print(product_of_numbers)
---
+++ sum_of_the_number.py
@@ -0,0 +1,8 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+# input number 1
+first_number = float(input("Enter the first number: "))
+# input number 2
+second_number = float(input("Enter the second number: "))
+# make a program that will sum the two numbers and print the answer
+sum_of_numbers = first_number + second_number
+print(sum_of_numbers)
