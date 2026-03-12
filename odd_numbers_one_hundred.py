4fb4509e781ad8a7dca7ccf8bfb3243a8261c3fb
james malitan <jamesmalitan21@gmail.com>
2026-03-12 13:22:13 +0800

    prog 18

---
+++ odd_numbers_one_hundred.py
@@ -0,0 +1,7 @@
+#Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+#make a range 0 - 100
+odd_number = 0
+for i in range(0, 101):
+# make a program to analyze if the number is odd
+  if i % 2 != 0:
+   print(i)
